92289830423c42ba90a7b8af6a41cda58c47707a
Ramil Khanov <sa-msk@yandex.ru>
2023-05-19 15:21:50 +0300

    Initial commit

---
+++ src/main/java/ru/java_khan/lesson/javafx/springboot/adressbookspringboot/AdressBookSpringBootApplication.java
@@ -0,0 +1,13 @@
+package ru.java_khan.lesson.javafx.springboot.adressbookspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdressBookSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdressBookSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/java_khan/lesson/javafx/springboot/adressbookspringboot/AdressBookSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.java_khan.lesson.javafx.springboot.adressbookspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdressBookSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
